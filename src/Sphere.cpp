504191e3427f1fdab2fcf010628eda445ba471f5
Lennart Vogelsang <dev@vogelsangs.net>
2017-12-21 20:50:14 +0100

    Reverted Sphere.cpp changes

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -1,22 +1,18 @@
 #include "Sphere.hpp"
 #include <iostream>
 
-Vertex nv(glm::vec3 position, glm::vec2 texture) {
-  return Vertex {.position = position, .texture = texture};
-}
-
-void Sphere::subdivide(Vertex a, Vertex b, Vertex c, int depth) {
+void Sphere::subdivide(glm::vec3 a, glm::vec3 b, glm::vec3 c, int depth) {
   if (depth == 0) {
-    vertices.push_back(a);
-    vertices.push_back(b);
-    vertices.push_back(c);
-    indices.push_back(vertices.size() - 3);
-    indices.push_back(vertices.size() - 2);
-    indices.push_back(vertices.size() - 1);
+    vectors.push_back(a);
+    vectors.push_back(b);
+    vectors.push_back(c);
+    indices.push_back(vectors.size() - 3);
+    indices.push_back(vectors.size() - 2);
+    indices.push_back(vectors.size() - 1);
   } else {
-    Vertex m1 = nv(midpoint(a.position, b.position), midpoint(a.texture, b.texture));
-    Vertex m2 = nv(midpoint(a.position, c.position), midpoint(a.texture, c.texture));
-    Vertex m3 = nv(midpoint(b.position, c.position), midpoint(b.texture, c.texture));
+    glm::vec3 m1 = midpoint(a, b);
+    glm::vec3 m2 = midpoint(a, c);
+    glm::vec3 m3 = midpoint(b, c);
     
     subdivide(a, m1, m2, depth-1);
     subdivide(m1, b, m3, depth-1);
@@ -27,68 +23,55 @@ void Sphere::subdivide(Vertex a, Vertex b, Vertex c, int depth) {
 
 
 void Sphere::reduce() {
-  std::vector<Vertex> new_vertices;
+  std::vector<glm::vec3> new_vectors;
   
-  for (size_t i = 0; i < vertices.size(); i++) {
+  for (size_t i = 0; i < vectors.size(); i++) {
     if (indices[i] == i) {
-      Vertex curr = vertices[i];
-      new_vertices.push_back(curr);
+      glm::vec3 curr = vectors[i];
+      new_vectors.push_back(curr);
     
-      for (size_t j = 0; j < vertices.size(); j++) {
-        Vertex dup = vertices[j];
-	if (glm::distance(curr.position, dup.position) < 0.001 && glm::distance(curr.texture, dup.texture) < 0.001) {
-	  indices[j] = new_vertices.size() - 1;
+      for (size_t j = 0; j < vectors.size(); j++) {
+	glm::vec3 dup = vectors[j];
+	if (glm::distance(curr, dup) < 0.001) {
+	  indices[j] = new_vectors.size() - 1;
 	}
       }
     }
   }
-  vertices = new_vertices;
+  vectors = new_vectors;
 }
 
 void Sphere::generateMesh() {
-  float r = radius;
-  // generate octahedron
-  // top view (order of triangles):
-  //      3
-  //   +-----+
-  //   |     |
-  // 4 |     | 2
-  //   |     |
-  //   +-----+
-  //      1
-  // up
-  subdivide(nv(glm::vec3(-r, 0, r), glm::vec2(0.0, 0.5)), nv(glm::vec3(0, r, 0), glm::vec2(0.125, 1)), nv(glm::vec3(r, 0, r), glm::vec2(0.25, 0.5)), detail);
-  subdivide(nv(glm::vec3(r, 0, r), glm::vec2(0.25, 0.5)), nv(glm::vec3(0, r, 0), glm::vec2(0.375, 1)), nv(glm::vec3(r, 0, -r), glm::vec2(0.5, 0.5)), detail);
-  subdivide(nv(glm::vec3(r, 0, -r), glm::vec2(0.5, 0.5)), nv(glm::vec3(0, r, 0), glm::vec2(0.625, 1)), nv(glm::vec3(-r, 0, -r), glm::vec2(0.75, 0.5)), detail);
-  subdivide(nv(glm::vec3(-r, 0, -r), glm::vec2(0.75, 0.5)), nv(glm::vec3(0, r, 0), glm::vec2(0.875, 1)), nv(glm::vec3(-r, 0, r), glm::vec2(1, 0.5)), detail);
-
-  // down
-  subdivide(nv(glm::vec3(-r, 0, r), glm::vec2(0.0, 0.5)), nv(glm::vec3(0, -r, 0), glm::vec2(0.125, 0)), nv(glm::vec3(r, 0, r), glm::vec2(0.25, 0.5)), detail);
-  subdivide(nv(glm::vec3(r, 0, r), glm::vec2(0.25, 0.5)), nv(glm::vec3(0, -r, 0), glm::vec2(0.375, 0)), nv(glm::vec3(r, 0, -r), glm::vec2(0.5, 0.5)), detail);
-  subdivide(nv(glm::vec3(r, 0, -r), glm::vec2(0.5, 0.5)), nv(glm::vec3(0, -r, 0), glm::vec2(0.625, 0)), nv(glm::vec3(-r, 0, -r), glm::vec2(0.75, 0.5)), detail);
-  subdivide(nv(glm::vec3(-r, 0, -r), glm::vec2(0.75, 0.5)), nv(glm::vec3(0, -r, 0), glm::vec2(0.875, 0)), nv(glm::vec3(-r, 0, r), glm::vec2(1, 0.5)), detail);
+  subdivide(glm::vec3(-radius, 0, -radius), glm::vec3(0, radius, 0), glm::vec3(radius, 0, -radius), detail);
+  subdivide(glm::vec3(radius, 0, -radius), glm::vec3(0, radius, 0), glm::vec3(radius, 0, radius), detail);
+  subdivide(glm::vec3(-radius, 0, -radius), glm::vec3(0, radius, 0), glm::vec3(-radius, 0, radius), detail);
+  subdivide(glm::vec3(-radius, 0, radius), glm::vec3(0, radius, 0), glm::vec3(radius, 0, radius), detail);
+  subdivide(glm::vec3(-radius, 0, -radius), glm::vec3(0, -radius, 0), glm::vec3(radius, 0, -radius), detail);
+  subdivide(glm::vec3(radius, 0, -radius), glm::vec3(0, -radius, 0), glm::vec3(radius, 0, radius), detail);
+  subdivide(glm::vec3(-radius, 0, -radius), glm::vec3(0, -radius, 0), glm::vec3(-radius, 0, radius), detail);
+  subdivide(glm::vec3(-radius, 0, radius), glm::vec3(0, -radius, 0), glm::vec3(radius, 0, radius), detail);
 }
 
 void Sphere::spherify() {
-  for (size_t i = 0; i < vertices.size(); i++) {
-    vertices[i].position = normalize(glm::vec3(0,0,0), vertices[i].position, radius);
+  for (size_t i = 0; i < vectors.size(); i++) {
+    vectors[i] = normalize(glm::vec3(0,0,0), vectors[i], radius);
   }
 }
 
-void Sphere::completeVertices() {
-  for (size_t i = 0; i < vertices.size(); i++) {
-    vertices[i].normal = vertices[i].position / radius;
-    vertices[i].color = color;
+void Sphere::generateVertices() {
+  for (size_t i = 0; i < vectors.size(); i++) {
+    vertices.push_back(Vertex {vectors[i], vectors[i] / radius, color});
   }
 }
 
 void Sphere::createShape() {
+  vectors.clear();
   vertices.clear();
   indices.clear();
   generateMesh();
-  //reduce();
+  reduce();
   spherify();
-  completeVertices();
+  generateVertices();
 }
 
 void Sphere::initGL() {
@@ -108,8 +91,6 @@ void Sphere::initGL() {
   glVertexAttribPointer(NORMAL_ID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(GLfloat) * 3));
   glEnableVertexAttribArray(COLOR_ID);
   glVertexAttribPointer(COLOR_ID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(GLfloat) * 6));
-  glEnableVertexAttribArray(TEXTURE_ID);
-  glVertexAttribPointer(TEXTURE_ID, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(GLfloat) * 9));
   // EBO
   glGenBuffers(1, &indexBuffer);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
