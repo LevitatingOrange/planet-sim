f072989d9682a942db79780527188590b771e1f7
Lennart Vogelsang <dev@vogelsangs.net>
2017-12-21 11:48:21 +0100

    Added flag for non-lambertian reflectors

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 - [x] player movement
 - [ ] config file parsing
 - [x] lighting
+- [ ] non lambertian reflectors
 - [ ] attentuation
 - [ ] ellipsoids instead of spheres
 - [ ] delta time in rendering and camera movement (maybe move input processing into update phase?)
--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -12,6 +12,7 @@ uniform vec3 viewPosition;
 uniform LightingParameters lightParameters;
 uniform LightingParameters materialParameters;
 uniform float shininess;
+uniform bool nonLambertian = false;
 
 in vec3 fragmentPosition;
 in vec3 fragmentNormal;
@@ -26,8 +27,16 @@ void main() {
   vec3 halfwayDirection = normalize(viewDirection + lightDirection);
   
   vec3 ambientLight = lightParameters.ambient * materialParameters.ambient;
-  vec3 diffuseLight = lightParameters.diffuse * materialParameters.diffuse * max(dot(normal, lightDirection), 0);
-  vec3 specularLight = lightParameters.specular * materialParameters.specular * pow(max(dot(normal, halfwayDirection), 0), shininess);
+  float diffuse = 0;
+  // non lambertian reflector (e.g. moon)
+  if (nonLambertian && dot(normal,lightDirection) > 0) {
+    diffuse = 1;
+  } else {
+    diffuse = max(dot(normal, lightDirection), 0);
+  }
+  vec3 diffuseLight = lightParameters.diffuse * materialParameters.diffuse * diffuse;
+  float specular = pow(max(dot(normal, halfwayDirection), 0), shininess);
+  vec3 specularLight = lightParameters.specular * materialParameters.specular * specular;
 
   color = (ambientLight + diffuseLight + specularLight) * fragmentColor;
  
